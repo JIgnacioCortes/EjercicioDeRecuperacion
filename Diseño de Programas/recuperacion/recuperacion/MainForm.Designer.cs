d5abf50bde9d18e946e73d623a89e630a3956e69
JIgnacioCortes <70727642+JIgnacioCortes@users.noreply.github.com>
2020-09-16 18:46:43 -0500

    Add files via upload

---
+++ Diseño de Programas/recuperacion/recuperacion/MainForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace recuperacion
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "País";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.BackColor = System.Drawing.SystemColors.Menu;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 89);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+			this.label2.Location = new System.Drawing.Point(12, 73);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(121, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Tipo de Datos";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.BackColor = System.Drawing.SystemColors.Menu;
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(12, 26);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 3;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 306);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(99, 32);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Graficar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(153, 12);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Legend = "Legend1";
+			series1.Name = "No. de casos";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(437, 300);
+			this.chart1.TabIndex = 5;
+			this.chart1.Text = "chart1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 211);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 6;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(12, 196);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Casos:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 163);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 8;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2TextChanged);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(12, 146);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Fecha:";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(12, 251);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Promedio:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(13, 268);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(99, 20);
+			this.textBox3.TabIndex = 11;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(416, 344);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(174, 77);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 13;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(31, 344);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(65, 86);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 14;
+			this.pictureBox3.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+			this.ClientSize = new System.Drawing.Size(612, 433);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "MainForm";
+			this.Text = "COVID-19";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		
+		
+	}
+}
---
+++ Diseño de Programas/recuperacion/recuperacion/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;	//Libreria para manipular los objetos del chart
+
+namespace recuperacion{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form{
+		
+		/*Atributos (variables globales)*/
+		// Crear una lista para almacenar los encabezados
+		List<string> hutt = new List<string>();
+		// Crear una lista para almacenar los paises
+		List<string> pais = new List<string>();
+		// Crear una lista para almacenar las fechas;
+		List<string> fecha = new List<string>();
+		// Lista para guardar todos los datos
+		List<string[]> data = new List<string[]>();
+		
+		/*Metodos personalizados*/
+		public void cambio(){
+			//Variables locales
+			
+			double promedio = 0.0;
+			long total = 0;
+			List<string> tmp = new List<string>();
+			
+			int i = comboBox1.SelectedIndex+2;
+			int j = comboBox2.SelectedIndex;
+			
+			foreach(string[] r in data){
+				tmp.Add(r[i]);
+				// FIXME: Se marca un error en la linea de abajo :( 
+				/* total += Convert.ToInt32(r[i]); //Obtener la suma de valores*/
+			}
+			
+			//Obtener Promedio 
+			// FIXME: Por el error de arriba no pude sacar el promedio :(
+			// promedio =(double)total / (double)tmp.Count;
+			
+			if(comboBox2.SelectedIndex != -1){
+				textBox1.Text = tmp[j];
+				textBox2.Text = fecha[j];
+				textBox3.Text = ":("; 
+			}
+		}
+		
+		public MainForm(){
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		void MainFormLoad(object sender, EventArgs e){
+			
+			//PARA LA GRAFICA
+			// Dar formato a la leyenda
+			chart1.Legends[0].BackColor = Color.AliceBlue;
+			chart1.Legends[0].BorderColor = Color.DarkBlue;
+			
+			// Variables locales
+			int j = 0;		// Contador de líneas
+			
+			// Lectura del archivo CSV
+			using(var lector = new StreamReader("covid.csv")){
+				// Hasta el fin del archivo
+				while(!lector.EndOfStream){
+					// Leer línea por línea ('\n')
+					var linia = lector.ReadLine();
+					
+					// Encabezado
+					if(j == 0){
+						// Recorrer elemento a elemento y añadir a una lista
+						foreach(string en in linia.Split(','))
+							hutt.Add(en);
+					}
+					else{
+						// Añadir los elementos de la tabla
+						data.Add(linia.Split(','));
+						// Se añaden los paises
+						pais.Add(linia.Split(',')[0] );
+						fecha.Add(linia.Split(',')[1]);
+					}
+					// contador de líneas leídas
+					j++;
+				}
+				
+				/* Leídos los datos, comenzar a llenar los espacios de la GUI */
+				hutt.RemoveRange(0,2);
+				foreach(string p in hutt)
+					comboBox1.Items.Add(p);
+				comboBox1.Text = comboBox1.Items[0].ToString();
+				
+				// Colocar los paises
+				foreach(string a in pais)
+					comboBox2.Items.Add(a);
+				comboBox2.Text = comboBox2.Items[0].ToString();
+				
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e){
+			chart1.Series["No. de casos"].Points.Clear();			//Limpiar la grafica
+			chart1.Series["No. de casos"].IsValueShownAsLabel = true;
+		}
+		
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e){
+			cambio();
+		}
+		
+		void ComboBox2SelectedIndexChanged(object sender, EventArgs e){
+			cambio();
+		}
+		
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
